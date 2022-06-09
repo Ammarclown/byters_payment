48591170e474537f93503ad98440b74d84b49bce
Ammarclown <ammar.mohamed@student.giu-uni.de>
2022-06-09 14:15:09 +0200

    payment f

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port=3000
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.cors;
 
 const stripe= require('stripe')('sk_test_51L2vu2FhZzaRvloxWe1usDutRKmio1kpgOIkRMZA2501HbOBg2OdKd7XnuYesH8V1WUSf1Un3LeW9eVdU1a9xnnN00HDr5xCei')
 
