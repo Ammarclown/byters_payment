82feeb56f82209d2697588bfe04add57e316b5dd
Ammarclown <ammar.mohamed@student.giu-uni.de>
2022-06-11 18:14:41 +0200

    yarab

--- index.js
+++ index.js
@@ -4,28 +4,43 @@ const axios = require('axios');
 //const { MongoClient } = require('./mongo');
 const req = require('express/lib/request');
 var bodyParser = require('body-parser')
-//const cors= require('cors')
+const cors= require('cors')
 const port=3000 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http:/localhost:3000');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next(); 
-});
 const stripe= require('stripe')('sk_test_51L2vu2FhZzaRvloxWe1usDutRKmio1kpgOIkRMZA2501HbOBg2OdKd7XnuYesH8V1WUSf1Un3LeW9eVdU1a9xnnN00HDr5xCei')
 
 
+app.get('/', async (req,res) => {
+  // console.log(stripe)
+   //const db = await MongoClient();
+   //if (!db) res.status(500).send('Systems Unavailable');
+   //console.log(stripe)
+   //const { data } = await axios.get('');
+   //await db.collection('myFirstDatabase').insertOne(data);
+   //return res.send(data);
+   const session = await stripe.checkout.sessions.create({
+     line_items: [
+       {
+         price_data: {
+           currency: 'usd',
+           product_data: {
+             name: "eggs",
+           },
+           unit_amount: (5*40*20),
+         },
+         quantity: 5,
+       },]
+     ,
+     mode: 'payment',
+     success_url: 'https://byters-shipping-microservice.vercel.app/shipments/4/',
+     cancel_url: 'http://localhost:3000/error/',
+   });
+   res.redirect(session.url)
+ });
 
+ 
 app.post('/payment',async (req,res) => {
   
     const session = await stripe.checkout.sessions.create({
@@ -48,33 +63,7 @@ app.post('/payment',async (req,res) => {
     res.redirect(session.url)
 //res.json({url: session.url})
   })
-app.get('/', async (req,res) => {
- // console.log(stripe)
-  //const db = await MongoClient();
-  //if (!db) res.status(500).send('Systems Unavailable');
-  //console.log(stripe)
-  //const { data } = await axios.get('');
-  //await db.collection('myFirstDatabase').insertOne(data);
-  //return res.send(data);
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price_data: {
-          currency: 'usd',
-          product_data: {
-            name: "eggs",
-          },
-          unit_amount: (5*40*20),
-        },
-        quantity: 5,
-      },]
-    ,
-    mode: 'payment',
-    success_url: 'https://byters-shipping-microservice.vercel.app/shipments/4/',
-    cancel_url: 'http://localhost:3000/error/',
-  });
-  res.redirect(session.url)
-});
+
 app.get('/trial', async (req,res) => {
   // console.log(stripe)
    //const db = await MongoClient();
