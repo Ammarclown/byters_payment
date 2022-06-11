4307393b14b098f3eb22f2340292e858b5b332d7
Ammarclown <ammar.mohamed@student.giu-uni.de>
2022-06-11 19:37:00 +0200

    p update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const stripe= require('stripe')('sk_test_51L2vu2FhZzaRvloxWe1usDutRKmio1kpgOIkRM
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http:/localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
