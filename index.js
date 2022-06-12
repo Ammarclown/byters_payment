c56cd0d22eb2da04cd713dbe99fbe0949b7ebde2
Ammarclown <ammar.mohamed@student.giu-uni.de>
2022-06-13 01:53:35 +0200

    payment

--- index.js
+++ index.js
@@ -13,19 +13,6 @@ const app = express();
 
 const stripe= require('stripe')('sk_test_51L2vu2FhZzaRvloxWe1usDutRKmio1kpgOIkRMZA2501HbOBg2OdKd7XnuYesH8V1WUSf1Un3LeW9eVdU1a9xnnN00HDr5xCei')
 app.use(express.json())
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   // Pass to next layer of middleware
-//   next();
-// });
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -96,7 +83,7 @@ app.post('/payment',async (req,res) => {
             product_data: {
               name: req.body.name,
             },
-            unit_amount: (req.body.quantity*req.body.unit_amount*20),
+            unit_amount: (req.body.unit_amount),
           },
           quantity: req.body.quantity,
         },]
